3d87c765cc0e5d3cd26fcc3c919482130097e71f
ahmedB7r <ahmedmagdyb7r@gmail.com>
2022-10-21 21:09:51 +0200

    auth:signin

--- src/api/shield/resolvers/mutation.ts
+++ src/api/shield/resolvers/mutation.ts
@@ -8,6 +8,13 @@ export const mutationsShield = {
     role: ['all'],
     permissions: allow,
   },
+  signin: {
+    name: 'signin',
+    slug: 'signin',
+    desc: '',
+    role: ['all'],
+    permissions: allow,
+  },
   signOut: {
     name: 'signOut',
     slug: 'signOut',
