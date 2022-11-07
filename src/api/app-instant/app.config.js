257e8e416e8be72b75073715233937cb6a3a76d5
ahmedB7r <ahmedmagdyb7r@gmail.com>
2022-11-07 10:17:10 +0200

    start the instant

---
+++ src/api/app-instant/app.config.js
@@ -0,0 +1,32 @@
+module.exports = {
+  expo: {
+    name: process.env.APP_NAME || 'test',
+    slug: 'app-builder',
+    version: process.env.APP_VERSION || '1.0.0',
+    orientation: 'portrait',
+    icon: 'https://aroundsketch.github.io/Apple-App-Icons/App%20Icon/Apple/AppStore/@PNG.png',
+    userInterfaceStyle: 'light',
+    splash: {
+      image: './assets/splash.png',
+      resizeMode: 'contain',
+      backgroundColor: '#ffffff',
+    },
+    updates: {
+      fallbackToCacheTimeout: 0,
+    },
+    assetBundlePatterns: ['**/*'],
+    ios: {
+      supportsTablet: true,
+    },
+    android: {
+      package: process.env.APP_VERSION || 'com.b7r.appbuilder',
+      adaptiveIcon: {
+        foregroundImage: './assets/adaptive-icon.png',
+        backgroundColor: '#FFFFFF',
+      },
+    },
+    web: {
+      favicon: './assets/favicon.png',
+    },
+  },
+}
