ab98978b61c799e9b895e7002a87445cba725b53
Hader Bustillo <haderbustillo@gmail.com>
2023-03-17 17:14:56 +0200

    add reset password

--- src/api/graphql/resolvers/mutation/auth.ts
+++ src/api/graphql/resolvers/mutation/auth.ts
@@ -15,6 +15,7 @@ import isEmail from 'validator/lib/isEmail'
 import makeSlug from 'slug-arabic'
 import axios from 'axios'
 import url from 'url'
+import sendSgMail, { MAILER_ITEMS } from '../../../utils/sgMail'
 
 function randomString(length, chars) {
   var mask = ''
@@ -504,127 +505,115 @@ export const Mutation = mutationType({
           return { user: isEmailExist, token }
         },
       })
-    //   t.field('requestReset', {
-    //     type: 'String',
-    //     args: {
-    //       type: arg({
-    //         type: enumType({
-    //           members: ['customer', 'vendor'],
-    //           name: 'userType',
-    //         }),
-    //         default: 'customer',
-    //       }),
-    //       email: stringArg(),
-    //       phone: stringArg(),
-    //     },
-    //     //@ts-ignore
-    //     async resolve(_root, { email, phone, type }, ctx) {
-    //       if (email && phone)
-    //         throw new Error('you must provide email or phone not both')
+    t.field('requestReset', {
+      type: 'String',
+      args: {
+        email: nonNull(stringArg()),
+      },
+      //@ts-ignore
+      async resolve(_root, { email }, ctx) {
+        const user = await ctx.db.user.findFirst({
+          select: { id: true },
+          where: {
+            email: { equals: email, mode: 'insensitive' },
+            builderDomain: { equals: ctx.builderDomain },
+          },
+        })
 
-    //       if (!email && !phone)
-    //         throw new Error('you must provide email or phone')
+        if (!user) {
+          return new Error(`No such user found for email ${email}`)
+        }
+        // 2. Set a reset token and expiry on that user
+        const resetToken = randomString(4, '#')
 
-    //       // 1. Check if this is a real user
-    //       if (email) {
-    //         const user = await ctx.db.user.findFirst({
-    //           select: { id: true },
-    //           where: {
-    //             email: { equals: email, mode: 'insensitive' },
-    //           },
-    //         })
+        const resetTokenExpiry = Date.now() + 3600000 // 1 hour from now
 
-    //         if (!user) {
-    //           return new Error(`No such user found for email ${email}`)
-    //         }
-    //         // 2. Set a reset token and expiry on that user
-    //         // const randomBytesPromiseified = promisify(randomBytes)
-    //         const resetToken = randomString(4, '#')
-    //         //  (await randomBytesPromiseified(20)).toString(
-    //         //   'hex',
-    //         // )
-    //         const resetTokenExpiry = Date.now() + 3600000 // 1 hour from now
-    //         const res = await ctx.db.user.update({
-    //           where: { id: user.id },
-    //           data: { resetToken, resetTokenExpiry },
-    //           select: { id: true },
-    //         })
+        await sendSgMail({
+          dynamic_template_data: { code: resetToken },
+          to: email,
+          templateId: MAILER_ITEMS.RESET_CODE,
+          subject: 'Reset Password',
+        })
 
-    //         // 3. Email them that reset token
-    //         // sendForgetPassByVendor(ctx, user, resetToken)
-    //       }
+        const res = await ctx.db.user.update({
+          where: { id: user.id },
+          data: { resetToken, resetTokenExpiry },
+          select: { id: true },
+        })
 
-    //       // 4. Return the message
-    //       return 'Thanks!'
-    //     },
-    //   })
-    // t.field('validateCode', {
-    //   type: 'AuthPayLoad',
-    //   args: {
-    //     code: nonNull(stringArg()),
-    //     email: stringArg(),
-    //     phone: stringArg(),
-    //     type: arg({ type: 'userType', default: 'customer' }),
-    //   },
-    //   //@ts-ignore
-    //   async resolve(_root, { code, email, phone, type }, ctx) {
-    //     if (email && phone)
-    //       throw new Error('you must provide email or phone not both')
+        // 4. Return the message
+        return 'Thanks!'
+      },
+    })
+    t.field('validateCode', {
+      type: 'AuthPayLoad',
+      args: {
+        code: nonNull(stringArg()),
+        email: nonNull(stringArg()),
+      },
+      //@ts-ignore
+      async resolve(_root, { code, email, phone, type }, ctx) {
+        const user = await ctx.db.user.findFirst({
+          // select: { id: true, name: true },
+          where: {
+            email: { equals: email, mode: 'insensitive' },
+            resetToken: code,
+            resetTokenExpiry: { gte: Date.now() - 3600000 },
+          },
+        })
+        if (!user) {
+          return new Error(`This Code is either invalid or expired!`)
+        }
 
-    //     if (!email && !phone) throw new Error('you must provide email or phone')
-    //     console.log('🚀 ~ file: auth.ts ~ line 666 ~ resolve ~ email', email)
+        const res = await ctx.db.user.update({
+          where: { id: user.id },
+          data: { resetToken: null, resetTokenExpiry: null },
+          select: { id: true },
+        })
 
-    //     // 1. Check if this is a real user
-    //     if (email) {
-    //       const user = await ctx.db.user.findFirst({
-    //         // select: { id: true, name: true },
-    //         where: {
-    //           email: { equals: email, mode: 'insensitive' },
-    //           resetToken: code,
-    //           resetTokenExpiry: { gte: Date.now() - 3600000 },
-    //         },
-    //       })
-    //       if (!user) {
-    //         return new Error(`This Code is either invalid or expired!`)
-    //       }
+        const token = ctx.auth.signInWithJWT(user)
 
-    //       const res = await ctx.db.user.update({
-    //         where: { id: user.id },
-    //         data: { resetToken: null, resetTokenExpiry: null },
-    //         select: { id: true,  },
-    //       })
+        return { token, user }
+      },
+    })
 
-    //       const token = ctx.auth.signInWithJWT(user)
+    t.field('resetPassword', {
+      type: 'User',
+      args: {
+        password: nonNull(stringArg()),
+      },
+      //@ts-ignore
+      async resolve(_root, { password }, ctx) {
+        // 3. Hash their new password
+        const hashed = await hashPassword(password)
+        if (!ctx?.user?.id) throw new Error('sorry please try again')
+        // 4. Save the new password to the user and remove old resetToken fields
+        const updatedUser = await ctx.db.user.update({
+          where: { id: ctx?.user?.id },
+          data: {
+            password: hashed,
+          },
+        })
 
-    //       return { token, user }
-    //     }
-
-    //     // 8. return the new user
-    //     return null
-    //   },
-    // })
-
-    // t.field('resetPassword', {
-    //   type: 'User',
-    //   args: {
-    //     password: nonNull(stringArg()),
-    //   },
-    //   //@ts-ignore
-    //   async resolve(_root, { password }, ctx) {
-    //     // 3. Hash their new password
-    //     const hashed = await hashPassword(password)
-    //     if (!ctx?.user?.id) throw new Error('sorry please try again')
-    //     // 4. Save the new password to the user and remove old resetToken fields
-    //     const updatedUser = await ctx.db.user.update({
-    //       where: { id: ctx?.user?.id },
-    //       data: {
-    //         password: hashed,
-    //       },
-    //     })
-
-    //     // 8. return the new user
-    //     return updatedUser
-    //   },
-    // })
+        // 8. return the new user
+        return updatedUser
+      },
+    })
   },
 })
+
+// sendSgMail({
+//   dynamic_template_data: { code: '0000' },
+//   to: 'ahmedmagdyb7r@gmail.com',
+//   templateId: MAILER_ITEMS.RESET_CODE,
+//   subject: 'Reset Password',
+// })
+//   .then((res) => {
+//     console.log('🚀 ~ file: auth.ts ~ line 35 ~ resolve ~ res', res)
+//   })
+//   .catch((err) => {
+//     console.log(
+//       '🚀 ~ file: auth.ts ~ line 35 ~ resolve ~ err',
+//       err.response.body.errors,
+//     )
+//   })
