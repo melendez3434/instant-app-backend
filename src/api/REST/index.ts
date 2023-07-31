d8cedadfea20fda71814e56dc17661290f573cc6
Ahmed Magdy <ahmedmagdyb7r@gmail.com>
2023-08-01 02:15:43 +0300

    deploy

--- src/api/REST/index.ts
+++ src/api/REST/index.ts
@@ -57,6 +57,8 @@ const router = (express) => {
 
   // This is your Stripe CLI webhook secret for testing your endpoint locally.
   const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET
+  const endpointSecretConnect = process.env.STRIPE_ENDPOINT_SECRET_CONNECT
+
   // const endpointSecret =
   //   'whsec_1d971589026671771d308dd2b4e2b2ca8697ac55d41edbbd700eeef1c107b13e'
 
@@ -75,96 +77,25 @@ const router = (express) => {
         // Otherwise use the basic event deserialized with JSON.parse
         console.log('🚀 ~ file: index.ts:27 ~ endpointSecret', endpointSecret)
 
-        // if (endpointSecret) {
-        //   // Get the signature sent by Stripe
-        //   const signature = request.headers['stripe-signature']
-        //   console.log('🚀 ~ file: index.ts:30 ~ signature', signature)
-        //   try {
-        //     event = stripe.webhooks.constructEvent(
-        //       request.body,
-        //       signature,
-        //       endpointSecret,
-        //     )
-        //   } catch (err: any) {
-        //     console.log(
-        //       `⚠️  Webhook signature verification failed.`,
-        //       err.message,
-        //     )
-        //     return response.sendStatus(400)
-        //   }
-        // }
-        let subscription
-        let status
-
-        // Handle the event
-        switch (event.type) {
-          case 'checkout.session.completed':
-            subscription = event.data.object
-            console.log(
-              '🚀 ~ file: index.ts ~ line 47 ~ subscription',
-              subscription,
+        if (endpointSecret) {
+          // Get the signature sent by Stripe
+          const signature = request.headers['stripe-signature']
+          console.log('🚀 ~ file: index.ts:30 ~ signature', signature)
+          try {
+            event = stripe.webhooks.constructEvent(
+              request.body,
+              signature,
+              endpointSecret,
             )
-            status = subscription.status
-            if (status == 'complete' && subscription.payment_status == 'paid') {
-              await prisma.app.update({
-                where: {
-                  id: Number(subscription.metadata.appId),
-                },
-                data: {
-                  stripeSubId: subscription.subscription,
-                  // owner: { update: { stripeCustomerId: subscription.customer } },
-                },
-              })
-            }
-            console.log(`Subscription status is ${status}.`)
-            // Then define and call a method to handle the subscription trial ending.
-            // handleSubscriptionTrialEnding(subscription);
-            break
-          case 'customer.subscription.trial_will_end':
-            subscription = event.data.object
-            status = subscription.status
-            console.log(`Subscription status is ${status}.`)
-            // Then define and call a method to handle the subscription trial ending.
-            // handleSubscriptionTrialEnding(subscription);
-            await handleSubscription(event.data.object)
-            await prisma.app.update({
-              where: { id: Number(subscription.metadata.appId) },
-              data: {
-                trialEndDate: new Date(),
-                isTrialEnd: true,
-              },
-            })
-
-            break
-          case 'customer.subscription.deleted':
-            subscription = event.data.object
+          } catch (err: any) {
             console.log(
-              '🚀 ~ file: index.ts ~ line 73 ~ subscription',
-              subscription,
+              `⚠️  Webhook signature verification failed.`,
+              err.message,
             )
-            status = subscription.status
-            console.log(`Subscription status is ${status}.`)
-            // Then define and call a method to handle the subscription deleted.
-            // handleSubscriptionDeleted(subscriptionDeleted);
-            await handleSubscription(event.data.object)
-
-            break
-          case 'customer.subscription.created':
-            await handleSubscription(event.data.object)
-
-            // Then define and call a method to handle the subscription created.
-            // handleSubscriptionCreated(subscription);
-            break
-          case 'customer.subscription.updated':
-            await handleSubscription(event.data.object)
-
-            // Then define and call a method to handle the subscription update.
-            // handleSubscriptionUpdated(subscription);
-            break
-          default:
-            // Unexpected event type
-            console.log(`Unhandled event type ${event.type}.`)
+            return response.sendStatus(400)
+          }
         }
+        await handleStripeEvent(event)
         // Return a 200 response to acknowledge receipt of the event
         response.send()
       } catch (error: any) {
@@ -175,7 +106,53 @@ const router = (express) => {
       }
     },
   )
+  express.post(
+    '/rest/stripe/webhook/connect',
+    app.raw({ type: 'application/json' }),
+    async (request, response) => {
+      try {
+        let event = request.body
+        console.log('🚀 ~ file: index.ts:69 ~ request.body:', request.body)
+        // Replace this endpoint secret with your endpoint's unique secret
+        // If you are testing with the CLI, find the secret by running 'stripe listen'
+        // If you are using an endpoint defined with the API or dashboard, look in your webhook settings
+        // at https://dashboard.stripe.com/webhooks
+        // Only verify the event if you have an endpoint secret defined.
+        // Otherwise use the basic event deserialized with JSON.parse
+        console.log(
+          '🚀 ~ file: index.ts:27 ~ endpointSecretConnect',
+          endpointSecretConnect,
+        )
 
+        if (endpointSecretConnect) {
+          // Get the signature sent by Stripe
+          const signature = request.headers['stripe-signature']
+          console.log('🚀 ~ file: index.ts:30 ~ signature', signature)
+          try {
+            event = stripe.webhooks.constructEvent(
+              request.body,
+              signature,
+              endpointSecretConnect,
+            )
+          } catch (err: any) {
+            console.log(
+              `⚠️  Webhook signature verification failed.`,
+              err.message,
+            )
+            return response.sendStatus(400)
+          }
+        }
+        await handleStripeEvent(event)
+        // Return a 200 response to acknowledge receipt of the event
+        response.send()
+      } catch (error: any) {
+        console.log('🚀 ~ file: index.ts:161 ~ error', error)
+        response.status(400).send({
+          message: error?.message || 'Look At Server Logs!',
+        })
+      }
+    },
+  )
   express.use(async (req, res, next) => {
     const ctx = await createContext({ req, res })
     req.ctx = ctx
@@ -261,6 +238,75 @@ const router = (express) => {
       res.status(500).send({ message: err.message })
     }
   })
+}
+
+const handleStripeEvent = async (event: Stripe.Event) => {
+  let subscription
+  let status
+
+  // Handle the event
+  switch (event.type) {
+    case 'checkout.session.completed':
+      subscription = event.data.object
+      console.log('🚀 ~ file: index.ts ~ line 47 ~ subscription', subscription)
+      status = subscription.status
+      if (status == 'complete' && subscription.payment_status == 'paid') {
+        await prisma.app.update({
+          where: {
+            id: Number(subscription.metadata.appId),
+          },
+          data: {
+            stripeSubId: subscription.subscription,
+            // owner: { update: { stripeCustomerId: subscription.customer } },
+          },
+        })
+      }
+      console.log(`Subscription status is ${status}.`)
+      // Then define and call a method to handle the subscription trial ending.
+      // handleSubscriptionTrialEnding(subscription);
+      break
+    case 'customer.subscription.trial_will_end':
+      subscription = event.data.object
+      status = subscription.status
+      console.log(`Subscription status is ${status}.`)
+      // Then define and call a method to handle the subscription trial ending.
+      // handleSubscriptionTrialEnding(subscription);
+      await handleSubscription(event.data.object)
+      await prisma.app.update({
+        where: { id: Number(subscription.metadata.appId) },
+        data: {
+          trialEndDate: new Date(),
+          isTrialEnd: true,
+        },
+      })
+
+      break
+    case 'customer.subscription.deleted':
+      subscription = event.data.object
+      console.log('🚀 ~ file: index.ts ~ line 73 ~ subscription', subscription)
+      status = subscription.status
+      console.log(`Subscription status is ${status}.`)
+      // Then define and call a method to handle the subscription deleted.
+      // handleSubscriptionDeleted(subscriptionDeleted);
+      await handleSubscription(event.data.object)
+
+      break
+    case 'customer.subscription.created':
+      await handleSubscription(event.data.object)
+
+      // Then define and call a method to handle the subscription created.
+      // handleSubscriptionCreated(subscription);
+      break
+    case 'customer.subscription.updated':
+      await handleSubscription(event.data.object)
+
+      // Then define and call a method to handle the subscription update.
+      // handleSubscriptionUpdated(subscription);
+      break
+    default:
+      // Unexpected event type
+      console.log(`Unhandled event type ${event.type}.`)
+  }
 }
 
 export default router
